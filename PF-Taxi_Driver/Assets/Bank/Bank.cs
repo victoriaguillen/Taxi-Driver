5c3818b4dfa114140072aa0f6fd1f0bb3b7a168e
202207977 <202207977@alu.comillas.edu>
2025-01-11 13:11:51 +0100

    [MODIFY] minor changes

--- PF-Taxi_Driver/Assets/Bank/Bank.cs
+++ PF-Taxi_Driver/Assets/Bank/Bank.cs
@@ -7,7 +7,7 @@ using UnityEngine.SceneManagement;
 
 public class Bank : MonoBehaviour
 {
-    [SerializeField] int startingBalance = 50;
+    [SerializeField] int startingBalance = 150;
 
     [SerializeField] int currentBalance;
     public int CurrentBalance { get { return currentBalance; } }
